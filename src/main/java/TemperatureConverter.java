a89e0483d782c1e7ae8a737d2104139e595e038e
gunner10z-school <24jzweiback@athenian.org>
2022-02-15 10:19:10 -0800

    asdf

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -8,7 +8,50 @@ import java.awt.event.ActionListener;
 public class TemperatureConverter {
 
     public TemperatureConverter() {
-        // YOUR CODE HERE!
+        JFrame frame = new JFrame("Temperature Converter");
+        frame.setLayout(new FlowLayout());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label1 = new JLabel("F");
+        frame.add(label1);
+
+        JTextField field1 = new JTextField(15);
+        frame.add(field1);
+
+        JButton button1 = new JButton("Convert to C");
+        frame.add(button1);
+
+        JLabel label2 = new JLabel("C");
+        frame.add(label2);
+
+        JTextField field2 = new JTextField(15);
+        frame.add(field2);
+
+        JButton button2 = new JButton("Convert to F");
+        frame.add(button2);
+
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                double input=Double.parseDouble(field1.getText());
+                String output=""+((input-32)/1.8);
+                field2.setText(output);
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                double input=Double.parseDouble(field2.getText());
+                String output=""+((input*(9/5))+32);
+                field1.setText(output);
+            }
+        });
+
+        // 4. Size the frame
+        frame.setSize(280, 300);
+
+        // 5. Show the frame
+        frame.setVisible(true);
     }
 
     public static void main(String args[]){
